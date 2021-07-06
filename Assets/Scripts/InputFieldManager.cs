5252d1f7319d2e523ff9f49080f487bfdc1b3f48
Nico <nicolas.bachleitner03@htl-salzburg.ac.at>
2021-07-06 16:02:01 +0200

    add own keyboard

---
+++ Assets/Scripts/InputFieldManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject keyboardGO;
+    public void ClickedInputField()
+    {
+        keyboardGO.SetActive(true);
+        keyboardGO.GetComponent<KeyboardScript>().TextField = GetComponent<InputField>();
+    }
+}
