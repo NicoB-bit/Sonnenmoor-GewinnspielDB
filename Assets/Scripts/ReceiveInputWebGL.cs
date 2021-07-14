235d968232c76b57bb257fd479f5872cd492cdee
Nico <nicolas.bachleitner03@htl-salzburg.ac.at>
2021-07-14 12:03:39 +0200

    bug

---
+++ Assets/Scripts/ReceiveInputWebGL.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReceiveInputWebGL : MonoBehaviour
+{
+    [SerializeField]
+    GameObject forenameGO;
+    [SerializeField]
+    GameObject surnameGO;
+    [SerializeField]
+    GameObject emailGO;
+    public void ReceiveInputDataWebGL(string value)
+    {
+        emailGO.GetComponent<InputField>().text = value;
+        if (value.StartsWith("1"))
+        {
+            forenameGO.GetComponent<InputField>().text = value;
+        }
+        else if (value.StartsWith("2"))
+        {
+            surnameGO.GetComponent<InputField>().text = value;
+        }
+        else
+        {
+            emailGO.GetComponent<InputField>().text = value;
+        }
+    }
+}
